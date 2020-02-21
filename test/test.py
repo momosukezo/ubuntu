4f597abd8986372f5a9a1cf2f298c7f53d601a58
momosukezo <y.matsumura1997@gmail.com>
2020-02-20 21:29:29 -0800

    python_test

--- test/test.py
+++ test/test.py
@@ -1,26 +1,26 @@
 import numpy as np
-
+# ファイルオープン
 def open_file(txt):
    f = open(txt)
    a = []
    for line in f:
-      line     = line.rstrip('\n')
-      line_sep = line.split(':')
+      line     = line.rstrip('\n')  # 改行コード削除
+      line_sep = line.split(':')    # コロン削除
       a.append(line_sep)
    return a
-
+# メイン処理
 def main_loop(txt):
    a   = []
    Sum = ''
    for i in range(len(txt)-1):
-      if int(txt[-1][0]) % int(txt[i][0]) == 0:
+      if int(txt[-1][0]) % int(txt[i][0]) == 0:          # 割り切れた場合
          a.append(txt[i])
          a_np   = np.array(a)
          a_sort = sorted(a_np,key=lambda x: int(x[0]))
          out    = np.array(a_sort)[:,1]
    for i in range(len(out)):
       Sum = Sum + out[i]
-   if a == []:
+   if a == []:                                           # 割り切れない場合
       b = txt[-1][0]
       return b
    return Sum
